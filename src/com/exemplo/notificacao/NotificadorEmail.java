d28789c36afc5c29f7b7e69f192913a9b73887e4
J0AO0 <102131443+J0AO0@users.noreply.github.com>
2023-10-10 15:03:32 -0300

    adição Notificação e Service de ativação

---
+++ src/com/exemplo/notificacao/NotificadorEmail.java
@@ -0,0 +1,13 @@
+package com.exemplo.notificacao;
+
+import com.exemplo.modelo.Cliente;
+
+public class NotificadorEmail {
+	private void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
+				cliente.getNome(), cliente.getEmail(), mensagem);
+
+	}
+}
+
+//pacote utilizado para fazer uma falsa notificação ao usuario sobre o estado de seu status.
---
+++ src/com/exemplo/service/AtivicaoClienteService.java
@@ -0,0 +1,14 @@
+package com.exemplo.service;
+
+import com.exemplo.modelo.Cliente;
+
+public class AtivicaoClienteService {
+	
+	private void ativar(Cliente cliente) {
+		cliente.ativar();
+		
+
+	}
+}
+
+//Classe de serviço utilizada para trocar o status do cliente de inativo para ativo
